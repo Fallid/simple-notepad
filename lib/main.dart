810789ac024289d99c062772b4dc8e01dc53600d
Ahmad Naufal <naufalopal002@gmail.com>
2025-05-10 14:45:02 +0700

    refactor: firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,20 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'package:get/get.dart';
 import 'package:responsive_framework/responsive_framework.dart';
 import 'package:simplenotepad/app/utils/dimension/screen_dimension.dart';
+import 'package:simplenotepad/firebase_options.dart';
 
 import 'app/routes/app_pages.dart';
 import 'generated/locales.g.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(
     ScreenUtilInit(
       designSize: const Size(360, 690),
