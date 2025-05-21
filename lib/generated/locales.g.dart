0ce67024f71a11fbd87871f136ad0adc3f390b3a
Fallid <naufalopal002@gmail.com>
2025-05-21 17:53:11 +0700

    style: :lipstick: Add new assets and localization

--- lib/generated/locales.g.dart
+++ lib/generated/locales.g.dart
@@ -4,36 +4,74 @@
 // ignore: avoid_classes_with_only_static_members
 class AppTranslation {
   static Map<String, Map<String, String>> translations = {
-    'en_US': Locales.en_US,
     'id_ID': Locales.id_ID,
+    'en_US': Locales.en_US,
   };
 }
 
 class LocaleKeys {
   LocaleKeys._();
-  static const buttons_login = 'buttons_login';
-  static const buttons_sign_in = 'buttons_sign_in';
-  static const buttons_logout = 'buttons_logout';
-  static const buttons_sign_in_fb = 'buttons_sign_in_fb';
-  static const buttons_sign_in_google = 'buttons_sign_in_google';
-  static const buttons_sign_in_apple = 'buttons_sign_in_apple';
+  static const buttons_auth_login = 'buttons_auth_login';
+  static const buttons_auth_register = 'buttons_auth_register';
+  static const buttons_auth_sign_in = 'buttons_auth_sign_in';
+  static const buttons_auth_logout = 'buttons_auth_logout';
+  static const buttons_auth_forgot_password = 'buttons_auth_forgot_password';
+  static const buttons_auth_sign_up = 'buttons_auth_sign_up';
+  static const header_auth_welcome_back = 'header_auth_welcome_back';
+  static const header_auth_greetings = 'header_auth_greetings';
+  static const header_auth_create_account = 'header_auth_create_account';
+  static const header_auth_information_field = 'header_auth_information_field';
+  static const body_auth_email = 'body_auth_email';
+  static const body_auth_password = 'body_auth_password';
+  static const body_auth_name = 'body_auth_name';
+  static const body_auth_dont_have_account = 'body_auth_dont_have_account';
+  static const body_auth_already_have_account =
+      'body_auth_already_have_account';
+  static const body_auth_hint_email = 'body_auth_hint_email';
+  static const body_auth_hint_password = 'body_auth_hint_password';
+  static const body_auth_hint_name = 'body_auth_hint_name';
 }
 
 class Locales {
-  static const en_US = {
-    'buttons_login': 'Login',
-    'buttons_sign_in': 'Sign-in',
-    'buttons_logout': 'Logout',
-    'buttons_sign_in_fb': 'Sign-in with Facebook',
-    'buttons_sign_in_google': 'Sign-in with Google',
-    'buttons_sign_in_apple': 'Sign-in with Apple',
-  };
   static const id_ID = {
-    'buttons_login': 'Masuk',
-    'buttons_sign_in': 'Masuk',
-    'buttons_logout': 'Keluar',
-    'buttons_sign_in_fb': 'Masuk dengan Facebook',
-    'buttons_sign_in_google': 'Masuk dengan Google',
-    'buttons_sign_in_apple': 'Masuk dengan Apple',
+    'buttons_auth_login': 'Masuk',
+    'buttons_auth_register': 'Daftar',
+    'buttons_auth_sign_in': 'Masuk di sini',
+    'buttons_auth_logout': 'Keluar',
+    'buttons_auth_forgot_password': 'Lupa Password',
+    'buttons_auth_sign_up': 'Daftar secara gratis',
+    'header_auth_welcome_back': 'Hai, selamat datang 👋',
+    'header_auth_greetings': 'Masukkan informasi yang telah kamu daftarkan',
+    'header_auth_create_account': 'Buat akun kamu',
+    'header_auth_information_field': 'Isi kolom di bawah ini untuk memulai',
+    'body_auth_email': 'Surel',
+    'body_auth_password': 'Sandi',
+    'body_auth_name': 'Nama',
+    'body_auth_dont_have_account': 'Tidak punya akun?',
+    'body_auth_already_have_account': 'Sudah memiliki akun?',
+    'body_auth_hint_email': 'contoh@mail.com',
+    'body_auth_hint_password': '*****',
+    'body_auth_hint_name': 'Jhon due',
+  };
+  static const en_US = {
+    'buttons_auth_login': 'Login',
+    'buttons_auth_register': 'Register',
+    'buttons_auth_sign_in': 'Sign in here',
+    'buttons_auth_logout': 'Logout',
+    'buttons_auth_forgot_password': 'Forgot Password',
+    'buttons_auth_sign_up': 'Sign up for free',
+    'header_auth_welcome_back': 'Hey, welcome back 👋',
+    'header_auth_greetings':
+        'Enter the information you entered while registering',
+    'header_auth_create_account': 'Create your account',
+    'header_auth_information_field': 'Enter the fields below to get started',
+    'body_auth_email': 'Email',
+    'body_auth_password': 'Password',
+    'body_auth_name': 'Name',
+    'body_auth_dont_have_account': 'Don\'t have account?',
+    'body_auth_already_have_account': 'Already have account?',
+    'body_auth_hint_email': 'example@mail.com',
+    'body_auth_hint_password': '*****',
+    'body_auth_hint_name': 'Jhon due',
   };
 }
