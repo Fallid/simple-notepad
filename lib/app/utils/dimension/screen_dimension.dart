19d54b56e037fd3f1b1bd3890ca447349306adee
Fallid <naufalopal002@gmail.com>
2025-05-27 23:03:57 +0700

    perf: :zap: improve responsive UI

--- lib/app/utils/dimension/screen_dimension.dart
+++ lib/app/utils/dimension/screen_dimension.dart
@@ -7,4 +7,11 @@ class ScreenDimension {
     const Breakpoint(start: 801, end: 1920, name: DESKTOP),
     const Breakpoint(start: 1921, end: double.infinity, name: '4K')
   ];
+
+  static List<Breakpoint> breakpointsLandscape = [
+    Breakpoint(start: 0, end: 480, name: MOBILE),
+    Breakpoint(start: 481, end: 900, name: TABLET),
+    Breakpoint(start: 901, end: 1400, name: DESKTOP),
+    Breakpoint(start: 1401, end: double.infinity, name: '4K'),
+  ];
 }
